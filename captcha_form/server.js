3f2e75579e417b6b4beeeee89c159ed9e37a9768
roshan04 <roshannikam04@gmail.com>
2023-03-05 02:12:59 +0530

    removed unnecessary changes

--- captcha_form/server.js
+++ captcha_form/server.js
@@ -1,16 +1,10 @@
 var express = require('express');
-const { fstat } = require('fs');
-const { inspect } = require('util');
 var app = express()
   , server = require('http').createServer(app)
 
 app.get('/', function(req, res) {
   console.log(`roshan1: bhai call aagyi h`);
-  res.sendFile("/Users/roshan/Documents/bstack_repos/sdk/captcha_form/index.html")
+  res.sendFile(__dirname + "/index.html")
 });
 
-app.post('/data', function(req, res) {
-  console.log(`roshan1: bhai call aagyi h ${inspect(req.data)}`);
-  res.sendFile("/Users/roshan/Downloads/myserver/index.html")
-});
 server.listen(8083);

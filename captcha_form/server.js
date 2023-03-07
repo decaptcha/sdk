d7ded01adfbfa5e00f37e09810cd0b2d9efba406
roshan04 <roshannikam04@gmail.com>
2023-03-07 14:42:56 +0530

    removed unnecessary changes

--- captcha_form/server.js
+++ captcha_form/server.js
@@ -3,7 +3,6 @@ var app = express()
   , server = require('http').createServer(app)
 
 app.get('/', function(req, res) {
-  console.log(`roshan1: bhai call aagyi h`);
   res.sendFile(__dirname + "/index.html")
 });
 
